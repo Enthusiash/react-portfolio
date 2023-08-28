031d75403a7def068bc70db9faae7ea4c3ac6bdb
Syntaxsh <ashleyotchengco@gmail.com>
2023-08-28 16:04:40 +0800

    re-design homepage

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,25 +7,28 @@ import  '../Styles/Home.css'
 
 // IMAGES //
 import Profile from '../assets/formalpic.png'
+import PHFlag from '../assets/philippines-flag-icon.png'
 
 const Header = () => {
 
   return (
     <div className="main-container">
 
+      <div className="left-container">
+        <div className="text">
+          <h1>Aspiring<br/>Front-End Web Developer</h1>
+          <h2>Hello! I am Otchengco, Ashley G. <span><img className="phflag" src={PHFlag} alt='Philippine Flag'></img></span></h2>
+        </div>
+
+        <div className="resume-btn">
+          <a href={ResumeBTN} target='_blank'>
+          <button className="resume">RESUME</button>
+          </a>
+        </div>
+      </div>
+
       <div className="ashlogo">
         <img className="ashash" src={Profile} alt='profile'></img>
-      </div>
-
-      <div className="text">
-        <h1>Hi, I'm Ashley G. Otchengco</h1>
-        <h2>Aspiring Front End Web Developer</h2>
-      </div>
-
-      <div className="resume-btn">
-        <a href={ResumeBTN} target='_blank'>
-        <button className="resume">RESUME</button>
-        </a>
       </div>
                    
     </div>
